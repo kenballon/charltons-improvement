6efa38c7f5e1db2f903f89d02b6f747761ddc2c9
Ken Ballon <bkenn003@gmail.com>
2024-07-02 16:22:39 +0800

    update

---
+++ dump.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="banner-image-wrapper">
+        <img src="[get_featured_img_url]" alt="image cover of [get_title]" fetchpriority="high" />
+        <h1 class="h1">[get_title]</h1>
+    </div>
+
+
+
+</body>
+
+</html>
